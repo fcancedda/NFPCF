a579026abae57530f3193ff7e13f20abc3abee53
cisco <fcancedda@albany.edu>
2021-10-23 10:22:30 -0400

    added GMF baseline

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from random import sample
+import tensorflow as tf
 
 
 class DataGenerator:
@@ -13,19 +14,17 @@ class DataGenerator:
         self.movies = set(self.df.mid.unique())
 
         self.train, self.test = self._train_test_split()
-        self.train = self.add_negatives(n_samples=4)
-        self.test_pos = self.load_test_data()
-        self.test_neg = self.load_negatives()
-
-
+        # self.train = self.add_negatives(self.df, n_samples=4)
+        # self.test_pos = self.load_test_data()
+        # self.test_neg = self.load_negatives()
 
     def _train_test_split(self):
         self.df.rating = np.int8(1)
-        self.df['latest'] = self.df.groupby(['uid'])['time'].rank(method='first', ascending=False)
+        self.df['latest'] = self.df.groupby(['uid'])['date'].rank(method='first', ascending=False)
         test = self.df[self.df.latest == 1]
         train = self.df[self.df.latest > 1]
-        return (train[['uid', 'mid', 'rating']].set_index('uid'),
-                test[['uid', 'mid', 'rating']].set_index('uid'))
+        return (train[['uid', 'mid', 'rating']],
+                test[['uid', 'mid', 'rating']])
 
     def add_negatives(self, df, n_samples=4):
         combine = df.groupby('uid')['mid'].apply(set).reset_index()
@@ -42,23 +41,30 @@ class DataGenerator:
 
     @staticmethod
     def load_ratings():
-        return pd.read_csv('MovieLens/ratings.dat',
-                           sep='::',
-                           header=None,
-                           names=['uid', 'mid', 'rating', 'date'],
-                           parse_dates=['date'],
-                           date_parser=lambda x: pd.to_datetime(x, unit='s', origin='unix'),
-                           engine='python')
-    @staticmethod
-    def get_features_as_list(df):
-        return df.apply(lambda x: [x.uid, x.mid], axis=1).tolist()
-
-    def get_test_negatives(self):
-        df = pd.read_csv('Data/ml-1m.test.negative',
-                         sep='\t',
+        df = pd.read_csv('MovieLens/ratings.dat',
+                         sep='::',
                          header=None,
-                         )
-        df.drop(columns=[0], inplace=True, axis=1)
-        df.reset_index(drop=True, inplace=True)
-        df.head()
-        return df.values.tolist()
+                         names=['uid', 'mid', 'rating', 'date'],
+                         parse_dates=['date'],
+                         date_parser=lambda x: pd.to_datetime(x, unit='s', origin='unix'),
+                         engine='python')
+        df.uid = df.uid - 1
+        df.mid = df.mid - 1
+        return df
+
+    @staticmethod
+    def get_features(df):
+        # return df.apply(lambda x: [x.uid, x.mid], axis=1).tolist()
+        # return df[['uid', 'mid']].to_numpy()
+        return [
+            df.uid.to_numpy(),
+            df.mid.to_numpy()
+        ]
+
+    @staticmethod
+    def get_dataset(df):
+        return tf.data.Dataset.from_tensor_slices(dict(df))
+
+    @staticmethod
+    def get_target(df):
+        return df.rating.to_numpy()
